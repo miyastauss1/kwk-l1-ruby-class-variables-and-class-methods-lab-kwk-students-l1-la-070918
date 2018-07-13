fa809751e0369da81a949bb8015aa6b3612e7611
Miya Stauss <miyastauss1@gmail.com>
2018-07-13 20:34:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,37 @@
+class Song
+    
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artists
+  end
+    
+
+def   
+  
+  
+  
+  
+  def Song.count
+    return @@count 
+  end 
+  
+  def Song.genres
+    # control duplicates somehow 
+    return @@genres 
+  end 
+  
+  
+  
+  
+  
 
